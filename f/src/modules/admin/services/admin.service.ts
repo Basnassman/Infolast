ae41155d58c111e32870e8ec2137f59fa3d237e5
Ahmad4664 <ailihmod28@gmail.com>
2026-05-28 02:12:04 +0300

    importnew

--- f/src/modules/admin/services/admin.service.ts
+++ f/src/modules/admin/services/admin.service.ts
@@ -1,3 +1,14 @@
-export const getMerkleJobs = async (): Promise<any[]> => {
-  return [];
+import { prisma } from "@core/db/prisma";
+import { DistributionType } from "@prisma/client";
+
+export const getMerkleJobs = async () => {
+  return prisma.merkleJob.findMany({
+    where: {
+      distributionType: DistributionType.AIRDROP,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 50,
+  });
 };
