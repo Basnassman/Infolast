1b82b26afe3dbe15ef2675acd52f69d8acaca311
Ahmad4664 <ailihmod28@gmail.com>
2026-05-31 03:34:30 +0300

    importnew

--- f/src/core/utils/async-handler.ts
+++ f/src/core/utils/async-handler.ts
@@ -15,36 +15,16 @@ import {
  * global-error.middleware.ts
  */
 
-export const asyncHandler =
-  <
-    P = any,
-    ResBody = any,
-    ReqBody = any,
-    ReqQuery = any
-  >(
-    fn: (
-      req: Request<
-        P,
-        ResBody,
-        ReqBody,
-        ReqQuery
-      >,
-      res: Response<ResBody>,
-      next: NextFunction
-    ) => Promise<any>
-  ): RequestHandler<
-    P,
-    ResBody,
-    ReqBody,
-    ReqQuery
-  > => {
-    return (
-      req,
-      res,
-      next
-    ) => {
-      Promise.resolve(
-        fn(req, res, next)
-      ).catch(next);
-    };
+export const asyncHandler = (
+  fn: RequestHandler
+): RequestHandler => {
+  return (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    Promise.resolve(
+      fn(req, res, next)
+    ).catch(next);
   };
+};
