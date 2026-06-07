0bb9afe73683626d4f5ec9c7a06330b685e9e0ca
Ahmad4664 <ailihmod28@gmail.com>
2026-06-07 22:41:58 +0000

    importnt

--- f/src/core/config/contracts.ts
+++ f/src/core/config/contracts.ts
@@ -2,12 +2,12 @@ export const contracts = {
   sepolia: {
     chainId: 11155111,
     name: "Sepolia",
-    rpc: process.env.SEPOLIA_RPC,
-    token: process.env.TOKEN_ADDRESS,
-    airdrop: process.env.AIRDROP_ADDRESS,
-    vesting: process.env.VESTING_ADDRESS,
-    priceOracle: process.env.PRICE_ORACLE_ADDRESS,
-    sale: process.env.SALE_ADDRESS,
+    rpc: process.env.SEPOLIA_RPC || "https://ethereum-sepolia.core.chainstack.com/892a28be8ff030c1544ff6eaf9c6ef0f",
+    token: process.env.TOKEN_ADDRESS || "0x32e64b5f40139f36D46dfbEa20Ef7A7C1EA76721",
+    airdrop: process.env.AIRDROP_ADDRESS || "0x10C50DC51d14089D80866803A15Dce112B17f799",
+    vesting: process.env.VESTING_ADDRESS || "0x8597F2723335E61Cd1CC1C21c962Ff393685Ca98",
+    priceOracle: process.env.PRICE_ORACLE_ADDRESS || "0x13425A5F4A68eC0D660EC4Ea5E21d42BA8e686Db",
+    sale: process.env.SALE_ADDRESS || "0x864C2350945bd4E9F7097F76CED2B10fD0cD2D99",
   },
 } as const;
 
