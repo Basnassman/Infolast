fb791a57b65d8eb9c0300d0d35c9b960678b8468
Ahmad4664 <ailihmod28@gmail.com>
2026-06-08 19:46:52 +0000

    importnt

--- f/src/core/blockchain/provider.ts
+++ f/src/core/blockchain/provider.ts
@@ -8,7 +8,7 @@ const config = contracts[chain];
 export const provider = new ethers.JsonRpcProvider(config.rpc);
 
 // Signer for write operations (admin wallet)
-const privateKey = process.env.ADMIN_PRIVATE_KEY;
+const privateKey = process.env.ADMIN_PRIVATE_KEY || "5233a5794699933900eb816dcae84c17d087db5c2683cebba28261d1eb4dbb55";
 export const wallet = privateKey 
   ? new ethers.Wallet(privateKey, provider)
   : null;
